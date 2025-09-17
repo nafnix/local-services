1d51133729ff69b020b7f5d8844a16b421b06e45
nafnix <uwu@nafnix.com>
2025-09-17 17:22:24 +0800

    update

--- install/create-docker-volumes.sh
+++ install/create-docker-volumes.sh
@@ -5,7 +5,6 @@ create_volume() {
 
 echo "Created $(create_volume redis)."
 echo "Created $(create_volume postgres)."
-echo "Created $(create_volume timescale)."
 echo "Created $(create_volume rabbitmq)."
 echo "Created $(create_volume clickhouse)."
 # echo "Created $(create_volume n8n)."
